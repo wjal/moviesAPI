b65afd5e14f4aac38066b5dfb544a8b6c72350d6
wjal <wjalittle@myseneca.ca>
2023-02-03 17:08:09 -0500

    added Plot Check

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 /*********************************************************************************
-*  BTI425 – Assignment 1
+*  BTI425 – Assignment 2
 *  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  
 *  No part of this assignment has been copied manually or electronically from any other source
 *  (including web sites) or distributed to other students.
 * 
-*  Name: James Little Student ID: 028496123 Date: 20/01/2023
+*  Name: James Little Student ID: 028496123 Date: 3/2/2023
 *  Cyclic Link: https://troubled-lamb-vestments.cyclic.app/
 *
 ********************************************************************************/ 
